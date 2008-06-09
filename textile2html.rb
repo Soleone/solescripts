9fffe9b807227b182a9804c50b5d3eb5e91a6fe0
Soleone <soleone@gmail.com>
2008-06-09 02:42:06 +0200

    Added some comments to the code.

--- textile2html.rb
+++ textile2html.rb
@@ -1,12 +1,16 @@
 require 'rubygems'
 require 'redcloth'
 
+# Check if 2 arguments have been supplied, else quit with usage message
 input, output = ARGV[0], ARGV[1]
 exit "Usage: textile2html SOURCE_FILE_NAME TARGET_FILE_NAME" unless input && output
 input, output = File.expand_path(input), File.expand_path(output)
 
-textile = File.new(input).readlines.join("")
+# Read the input file (textile)
+textile = File.new(input).readlines.join('')
+# Write the output file (converted html)
 File.open(output, 'w') do |file|
 	file.write(RedCloth.new(textile).to_html)
 end
+
 puts "Successfully converted '#{input}' to '#{output}'"
